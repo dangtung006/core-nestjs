dde48e0d645bc0478f65eab0cfc14b325cecd09b
dangtung <language0512@gmail.com>
2023-04-07 22:50:36 +0700

    update role controllers

--- src/modules/roles/controllers/role.admin.controller.ts
+++ src/modules/roles/controllers/role.admin.controller.ts
@@ -5,6 +5,7 @@ import {
     Put,
     Delete,
 
+    Query,
     Body,
     Controller,
 
@@ -15,21 +16,43 @@ import {
 } from '@nestjs/common';
 
 import { RoleService } from '../services/role.service';
+import { RoleEntity } from 'src/models/entities/role.entity';
 
 @Controller("role")
 export class RoleAdminController {
     constructor(
+        private readonly roleService: RoleService,
         // private readonly paginationService: PaginationService,
         // private readonly permissionService: PermissionService,
-        private readonly roleService: RoleService,
         // private readonly roleEnumService: RoleEnumService
     ) { }
 
 
     @Get('list')
     async list(): Promise<any> {
-        console.log("aaaaaaaaa");
-        return "aaaaaa"
-        // const roles: RoleEntity[] = await this.roleService.findAll({});
+        const roles: RoleEntity[] = await this.roleService.findAll({});
+        return roles;
+    }
+
+    @Post("user")
+    async create(): Promise<RoleEntity> {
+        const role = await this.roleService.create({ name: "admin" });
+        return role;
+    }
+
+    @Put("user/active")
+    async block(
+        @Query("id") id: string
+    ): Promise<RoleEntity> {
+        let result = await this.roleService.active(id);
+        return result;
+    }
+
+    @Put("user/inactive")
+    async unblock(
+        @Query("id") id: string
+    ): Promise<RoleEntity> {
+        let result = await this.roleService.inactive(id);
+        return result;
     }
 }
